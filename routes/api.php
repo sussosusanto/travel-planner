39009eedd71936a59bf94083ffaba595b5ab366d
Heri Susanto <sussosusanto@gmail.com>
2024-08-22 18:00:54 +0700

    add Travel model and controller basic

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\TravelController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// User Authentication Routes
+Route::post('/login', [UserController::class, 'login']);
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/logout', [UserController::class, 'logout'])->middleware('auth:sanctum');
+
+// Travel Management Routes
+Route::get('/travels', [TravelController::class, 'index'])->middleware('auth:sanctum');
+Route::get('/travels/{travel}', [TravelController::class, 'show'])->middleware('auth:sanctum');
+Route::post('/travels', [TravelController::class, 'store'])->middleware('auth:sanctum');
+Route::put('/travels/{travel}', [TravelController::class, 'update'])->middleware('auth:sanctum');
+Route::delete('/travels/{travel}', [TravelController::class, 'destroy'])->middleware('auth:sanctum');
